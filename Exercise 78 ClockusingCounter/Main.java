d94a2d77316df04c86ac96de8fef72caca66ef7d
Ryan Guo <guohao_lin@msn.com>
2018-12-04 23:26:34 -0800

    Exercise 78 Clock using a counter

---
+++ Exercise 78 ClockusingCounter/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        BoundedCounter seconds = new BoundedCounter(59);
+        BoundedCounter minutes = new BoundedCounter(59);
+        BoundedCounter hours = new BoundedCounter(23);
+
+        // read the initial value of seconds from the user
+        System.out.println("seconds: ");
+        int s = Integer.parseInt(reader.nextLine());
+
+        // read the initial value of minutes from the user
+        System.out.println("minutes: ");
+        int m = Integer.parseInt(reader.nextLine());
+
+        // read the initial value of hours from the user
+        System.out.println("hours: ");
+        int h = Integer.parseInt(reader.nextLine());
+
+        seconds.setValue(s);
+        minutes.setValue(m);
+        hours.setValue(h);
+
+        int i = 0;
+        while (i < 121) {
+            System.out.println(hours + ":" + minutes + ":" + seconds);   // the current time printed
+            
+            seconds.next();
+            
+            if (seconds.getValue() == 0) {
+                minutes.next();
+                if (minutes.getValue() == 0) {
+                    hours.next();
+                }
+            }
+
+            i++;
+        }
+    }
+}
