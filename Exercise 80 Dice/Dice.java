1f9bc138c1adc871a3d6440de67870e793bd3f4b
Ryan Guo <guohao_lin@msn.com>
2018-12-05 23:55:29 -0800

    Update Dice.java
    In the template is class Dice that has the following functionality:
    
    The constructor Dice(int numberOfSides) creates a new dice object that has the number of sides defined by the argument.
    The method roll tells the result of a roll (which depends on the number of its sides)

--- Exercise 80 Dice/Dice.java
+++ Exercise 80 Dice/Dice.java
@@ -3,12 +3,12 @@ import java.util.Random;
 public class Dice {
     private Random random = new Random();
     private int numberOfSides;
-
+    //creates a new dice object
     public Dice(int numberOfSides) {
         this.numberOfSides = numberOfSides;
         random = new Random();
     }
-
+    //The method roll tells the result of a roll (which depends on the number of its sides)
     public int roll() {
         // create here a random number belonging to range 1-numberOfSided
 
