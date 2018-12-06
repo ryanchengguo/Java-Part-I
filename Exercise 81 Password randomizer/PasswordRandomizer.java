ea41bb3ba908275ba3ee9894be4beabb9596b9a9
Ryan Guo <guohao_lin@msn.com>
2018-12-05 23:58:31 -0800

    Update PasswordRandomizer.java

--- Exercise 81 Password randomizer/PasswordRandomizer.java
+++ Exercise 81 Password randomizer/PasswordRandomizer.java
@@ -2,12 +2,14 @@ import java.util.Random;
 
 public class PasswordRandomizer {
     private int length;
-
+    
+    //The constructor PasswordRandomizer creates a new object, which uses the given password length.
     public PasswordRandomizer(int length) {
         this.length = length;
 
     }
-
+    
+    //The method createPassword returns a new password, which consists of symbols a-z and is of the length given as a parameter to the constructor
     public String createPassword() {
         Random random  = new Random();
         String password = "";
