2c2c40ad43d62411e4927280d57ce8f35a2a55ae
Ryan Guo <guohao_lin@msn.com>
2018-12-08 23:46:31 -0800

    Exercise 87 ApartmentComparison

---
+++ Exercise 87 ApartmentComparison/Apartment.java
@@ -0,0 +1,39 @@
+
+public class Apartment {
+
+    private int rooms;
+    private int squareMeters;
+    private int pricePerSquareMeter;
+    private int priceOfApartment;
+
+    public Apartment(int rooms, int squareMeters, int pricePerSquareMeter) {
+        this.rooms = rooms;
+        this.squareMeters = squareMeters;
+        this.pricePerSquareMeter = pricePerSquareMeter;
+        this.priceOfApartment = pricePerSquareMeter * squareMeters;
+    }
+
+    public boolean larger(Apartment otherApartment){
+        if (this.squareMeters> otherApartment.squareMeters){
+            return true;
+        }
+        else return false;
+    }
+
+    public int priceDifference(Apartment otherApartment){
+        if (this.priceOfApartment>otherApartment.priceOfApartment){
+            return this.priceOfApartment - otherApartment.priceOfApartment;
+        }
+        else return otherApartment.priceOfApartment - this.priceOfApartment;
+
+    }
+
+    public boolean moreExpensiveThan(Apartment otherApartment) {
+        if (this.priceOfApartment > otherApartment.priceOfApartment) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+}
