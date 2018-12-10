813d8b12be2134be76ca5864aa4fe80ce07d792f
Ryan Guo <guohao_lin@msn.com>
2018-12-09 21:01:40 -0800

    Update RecurringWordAnswer.java

--- Exercise 91 Extending My Date/RecurringWordAnswer.java
+++ Exercise 91 Extending My Date/RecurringWordAnswer.java
@@ -18,5 +18,20 @@ public class MyDate {
     public String toString() {
         return this.day + "." + this.month + "." + this.year;
     }
+    
+    public boolean earlier(MyDate compared) {
+        if (this.year < compared.year) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month < compared.month) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month == compared.month
+                && this.day < compared.day) {
+            return true;
+        }
+
  
 }
