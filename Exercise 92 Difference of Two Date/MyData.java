2f4056d304a0a6c164f05c93d2d4a2cd8e93e04c
Ryan Guo <guohao_lin@msn.com>
2018-12-10 21:49:48 -0800

    Update MyData.java

--- Exercise 92 Difference of Two Date/MyData.java
+++ Exercise 92 Difference of Two Date/MyData.java
@@ -8,3 +8,21 @@ public class MyDate {
         this.month = montd;
         this.year = year;
     }
+    
+public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean earlier(MyDate compared) {
+        if (this.year < compared.year) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month < compared.month) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month == compared.month
+                && this.day < compared.day) {
+            return true;
+        }
