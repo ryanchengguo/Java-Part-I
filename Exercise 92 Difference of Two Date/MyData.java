31f0818dd5d6945a78dc169a04636f3fac0f6271
Ryan Guo <guohao_lin@msn.com>
2018-12-10 21:49:32 -0800

    Update MyData.java

--- Exercise 92 Difference of Two Date/MyData.java
+++ Exercise 92 Difference of Two Date/MyData.java
@@ -1,4 +1,10 @@
+public class MyDate {
+    private int day;
+    private int month;
+    private int year;
 
-public class MyData {
-
-}
+    public MyDate(int day, int montd, int year) {
+        this.day = day;
+        this.month = montd;
+        this.year = year;
+    }
