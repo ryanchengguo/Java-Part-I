c5db3a9af1a9346d2b3f5290893c12594c716d20
Ryan Guo <guohao_lin@msn.com>
2018-12-10 21:51:19 -0800

    Update Main.java

--- Exercise 92 Difference of Two Date/Main.java
+++ Exercise 92 Difference of Two Date/Main.java
@@ -2,3 +2,4 @@ public class Main {
     public static void main(String[] args) {
         MyDate first = new MyDate(24, 12, 2009);
         MyDate second = new MyDate(1, 1, 2011);
+        MyDate third = new MyDate(25, 12, 2010);
