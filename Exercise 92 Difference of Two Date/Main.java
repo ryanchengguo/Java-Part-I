2932cf19a5da967d7e45872eb8588c7517b70bbc
Ryan Guo <guohao_lin@msn.com>
2018-12-10 21:51:32 -0800

    Update Main.java

--- Exercise 92 Difference of Two Date/Main.java
+++ Exercise 92 Difference of Two Date/Main.java
@@ -3,3 +3,10 @@ public class Main {
         MyDate first = new MyDate(24, 12, 2009);
         MyDate second = new MyDate(1, 1, 2011);
         MyDate third = new MyDate(25, 12, 2010);
+        System.out.println( second + " and " + first + " difference in years: " + second.differenceInYears(first) );
+
+        System.out.println( third + " and " + first + " difference in years: " + third.differenceInYears(first) );
+
+        System.out.println( second + " and " + third + " difference in years: " + second.differenceInYears(third) );
+    }
+}
