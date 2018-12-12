7fd0c22e641ae396218dbf0552b108cd5aa8e65b
Ryan Guo <guohao_lin@msn.com>
2018-12-12 00:26:24 -0800

    Update Person.java

--- Exercise 93 Person extended/Person.java
+++ Exercise 93 Person extended/Person.java
@@ -1,4 +1,10 @@
+import java.util.Calendar;
 
 public class Person {
-
-}
+    private String name;
+    private MyDate birthday;
+    
+    public Person(String name, int pp, int kk, int vv) {
+        this.name = name;
+        this.birthday = new MyDate(pp, kk, vv);
+    }
