fd367522e46b22b9fdb527c72a5173879abc5acf
Ryan Guo <guohao_lin@msn.com>
2018-12-12 00:25:51 -0800

    Update MyData.java

--- Exercise 93 Person extended/MyData.java
+++ Exercise 93 Person extended/MyData.java
@@ -9,3 +9,24 @@ public class MyDate {
         this.month = kk;
         this.year = vv;
     }
+
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean earlier(MyDate compared) {
+        if (this.year < compared.year) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month < compared.month) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month == compared.month
+                && this.day < compared.day) {
+            return true;
+        }
+
+        return false;
+    }
