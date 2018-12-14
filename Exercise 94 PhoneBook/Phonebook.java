e21c5fc60ad61263da31c0cf7174a338cdf9e464
Ryan Guo <guohao_lin@msn.com>
2018-12-13 23:46:47 -0800

    Update Phonebook.java

--- Exercise 94 PhoneBook/Phonebook.java
+++ Exercise 94 PhoneBook/Phonebook.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 
 public class Phonebook {
+    private ArrayList<Person> phoneBook;
 
-}
+    public Phonebook() {
+        this.phoneBook = new ArrayList<Person>();
+    }
+  
+  public void printAll(){
+        for (Person person:phoneBook
+             ) {
+            System.out.println(person);
+
+        }
+    }
