968399e578f9e97930812e097ad44998f22fc21e
Ryan Guo <guohao_lin@msn.com>
2018-12-14 23:51:58 -0800

    Update Money.java

--- Exercise 95 Money/Money.java
+++ Exercise 95 Money/Money.java
@@ -1,4 +1,11 @@
-
 public class Money {
 
-}
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+
+        if (cents > 99) {
+            euros += cents / 100;
+            cents %= 100;
+        }
